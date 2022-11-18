a68d84fe5bd5abaca2ab82ba779643def2d47682
Cedrik Nguefack <cedric.nguefack@tapmeppe.com>
2022-11-18 06:51:11 +0100

    correction of bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,7 @@ const Home: NextPage = () => {
     <Head>
       <title>DESIGN 4 Green</title>
       <meta name="description" content="Design 4 Green Application" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/site.webmanifest"></link>
+      <link rel="icon" type="image/svg" href="/favicon-32x32.png" />
     </Head>
     <Suspense fallback={`Loading...`}>
       <Main />
