057cf2679f1daa2edf3db502103ef98bbd7b6f35
Cedrik Nguefack <cedric.nguefack@tapmeppe.com>
2022-11-18 06:27:06 +0100

    correction of bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,8 @@ import type { NextPage } from 'next'
 import dynamic from 'next/dynamic'
 import { Suspense } from 'react'
 import Head from 'next/head'
-//import PlacesModel, { GoodPractices as GoodPracticesType } from '../src/models/Places'
 
-const Header = dynamic(() => import('../src/components/Header'), { suspense: true })
 const Main = dynamic(() => import('../src/components/Main'), { ssr: false })
-//const GoodPractices = dynamic(() => import('../src/components/index/GoodPractices'), { suspense: true })
-const Footer = dynamic(() => import('../src/components/Footer'), { suspense: true })
-const Map = dynamic(() => import('../src/components/Map'), { ssr: false })
 const Home: NextPage = () => {
 
   return <>
@@ -27,16 +22,3 @@ const Home: NextPage = () => {
 }
 
 export default Home
-
-/* export async function getStaticProps() {
-
-  const goodPractices = GoodPracticesModel.getGoodPractices(),
-    totalItems = GoodPracticesModel.getGoodPracticesSize()
-
-  return {
-    props: {
-      goodPractices,
-      totalItems
-    }
-  }
-} */
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
 import { NextComponentType } from 'next'
-import Link from 'next/link'
 
 
 const Footer: NextComponentType = () => {
